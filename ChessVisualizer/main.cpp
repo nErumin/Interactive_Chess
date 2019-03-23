0bddcd04d7401c56e29c53fb278c8ad87c1b6c33
nErumin <nerumin90@gmail.com>
2019-03-23 18:40:32 +0900

    add test bed.

--- ChessVisualizer/main.cpp
+++ ChessVisualizer/main.cpp
@@ -1,11 +1,12 @@
-#include "MainWindow.h"
+#include "ChessWindow.h"
+#include "ChessController.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    ChessController e;
 
+    e.startChess();
     return a.exec();
 }
