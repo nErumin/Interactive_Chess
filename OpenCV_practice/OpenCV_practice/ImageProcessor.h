23be5c59ca628e52a7857e7da645ebba1ee44ae1
jwj <ehdwlsdnwls@naver.com>
2019-03-29 20:37:37 +0900

    make ImageProcessor header

--- OpenCV_practice/OpenCV_practice/ImageProcessor.h
+++ OpenCV_practice/OpenCV_practice/ImageProcessor.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef IMAGEPROCESSOR_H
+#define IMAGEPROCESSOR_H
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
+
+using namespace cv;
+using namespace std;
+
+class ImageProcessor {
+private:
+	Mat img;
+
+public:
+	ImageProcessor();
+	~ImageProcessor() = default;
+	bool detectAndDrawChessboardCorners(String img_name);
+};
+#endif
