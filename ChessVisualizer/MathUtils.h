5afa29427485f0df8ab3346a92a991e106bdd1b3
nErumin <nerumin90@gmail.com>
2019-04-06 15:27:43 +0900

    fix wrong random number generation.

--- ChessVisualizer/MathUtils.h
+++ ChessVisualizer/MathUtils.h
@@ -12,13 +12,15 @@ constexpr double degreeToRadian(double degree)
     return degree * (PI / 180.0);
 }
 
+#include <iostream>
 template <typename T>
 T pickRandomNumber(T minInclusive, T maxExclusive)
 {
     using TDistribution = std::uniform_int_distribution<T>;
 
-    static thread_local std::default_random_engine engine{ static_cast<unsigned int>(time(nullptr)) };
-    static TDistribution distribution;
+    static std::random_device device;
+    static thread_local std::default_random_engine engine{ device() };
+    static thread_local TDistribution distribution;
 
     return distribution(engine, TDistribution::param_type(minInclusive, maxExclusive - 1));
 }
