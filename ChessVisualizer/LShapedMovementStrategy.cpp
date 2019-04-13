15d21eb5236fb621fa207dc83deda5907e1b8982
nErumin <nerumin90@gmail.com>
2019-04-13 17:47:13 +0900

    Add obstacles to LShapedMovementStrategy.

--- ChessVisualizer/LShapedMovementStrategy.cpp
+++ ChessVisualizer/LShapedMovementStrategy.cpp
@@ -19,8 +19,28 @@ std::vector<Vector2> LShapedMovementStrategy::movableLocations(const Vector2& ce
     return locations;
 };
 
-ObstacleMovablePair LShapedMovementStrategy::movableLocationsUsingObstacles(const Vector2& centerLocation, [[maybe_unused]] std::vector<std::deque<bool>> obstacleMap) const noexcept
+ObstacleMovablePair LShapedMovementStrategy::movableLocationsUsingObstacles(const Vector2& centerLocation, std::vector<std::deque<bool>> obstacleMap) const noexcept
 {
-    // no obstacles
-    return { {}, movableLocations(centerLocation) };
+    std::vector<Vector2> obstacles;
+    std::vector<Vector2> locations;
+
+    auto movable = movableLocations(centerLocation);
+
+    for (const auto& location : movable)
+    {
+        auto normalized = normalizeToIntegerVector(location);
+
+        if (normalized.first < obstacleMap.size() &&
+            normalized.second < obstacleMap.front().size() &&
+            obstacleMap[normalized.first][normalized.second])
+        {
+            obstacles.push_back(location);
+        }
+        else
+        {
+            locations.push_back(location);
+        }
+    }
+
+    return { obstacles, locations };
 }
