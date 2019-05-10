221f86c6953a737c1aa3893beeb31942efb06feb
jwj <ehdwlsdnwls@naver.com>
2019-05-11 00:02:21 +0900

    main for test

--- OpenCV_practice/OpenCV_practice/main.cpp
+++ OpenCV_practice/OpenCV_practice/main.cpp
@@ -1,8 +1,15 @@
 #include "OpenCVController.h"
 
 int main() {
+	/*
 	OpenCVController controller;
 	controller.startVideo(1);
-
+	*/
+	
+	ImageProcessor ip;
+	String img_name = "test.jpg";
+	if (ip.isFirst()) ip.initialize(img_name);
+	else ip.recognizeMovement(img_name);
+	
 	return 0;
 }
