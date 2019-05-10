6873f491c092dd6838256283606b446f9c05432d
jwj <ehdwlsdnwls@naver.com>
2019-05-11 00:17:06 +0900

    measure execution time

--- OpenCV_practice/OpenCV_practice/main.cpp
+++ OpenCV_practice/OpenCV_practice/main.cpp
@@ -1,4 +1,5 @@
 #include "OpenCVController.h"
+#include <chrono>
 
 int main() {
 	/*
@@ -8,8 +9,13 @@ int main() {
 	
 	ImageProcessor ip;
 	String img_name = "test.jpg";
+	
+	chrono::system_clock::time_point start = chrono::system_clock::now();
+	
 	if (ip.isFirst()) ip.initialize(img_name);
 	else ip.recognizeMovement(img_name);
 	
+	chrono::duration<double> sec = chrono::system_clock::now() - start;
+	cout << "elapsed time : " << sec.count() << endl;
 	return 0;
 }
