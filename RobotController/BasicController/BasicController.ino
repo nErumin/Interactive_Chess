f32f643c226e8bafbf31b08aca5990dc92537aec
JunHyuckWoo <wjh0470@cau.ac.kr>
2019-05-20 00:53:55 +0900

    Add semi-demo

--- RobotController/BasicController/BasicController.ino
+++ RobotController/BasicController/BasicController.ino
@@ -6,7 +6,7 @@
 #endif   
 
 #define BAUDRATE  57600
-#define NCNT      1
+#define NCNT      5
 #define SCNT      0
 #define DXL_ID_1  1
 #define DXL_ID_2  2
@@ -19,9 +19,16 @@ DynamixelWorkbench dxl_wb;
 uint8_t dxl_id[5] = {DXL_ID_1, DXL_ID_2, DXL_ID_3, DXL_ID_4, DXL_ID_5};
 int32_t goal_position[2] = {0, 1023};
 int32_t init_position[5] = {2250, 2250, 2250, 2350, 1600};
+int32_t pause_position[5] = {2245, 900, 2740, 3160, 1900};
+int32_t grab_position[5] = {2530, 2100, 2350, 3300, 1900};
+int32_t trash_position[5] = {2700, 2100, 2350, 2300, 1900};
 int32_t present_position[5] = {0, 0, 0, 0, 0};
 int32_t led[2] = {0, 1};
 const uint8_t handler_index = 0;
+
+int loop_num = 0;
+int diff = 0;
+int e = 0;
 
 char bufferIndex = 0;
 char buffer[20];
@@ -85,18 +92,27 @@ void setup() {
             present_position[cnt] = get_data;
         }
     }
-    
-    // Set initial goalposition
-    for (int cnt=SCNT; cnt < NCNT; cnt++) {
-        dxl_wb.goalPosition(dxl_id[cnt], init_position[cnt]);
+
+    // Set init position
+    Serial.println("Init State");
+    loop_num = getMax(present_position, init_position, NCNT);
+    for (e = 0; e<loop_num; e+=10) {
+        for (int cnt=SCNT; cnt < NCNT; cnt++) {
+            if(present_position[cnt] < init_position[cnt]) { diff=10;}
+            else if(present_position[cnt] > init_position[cnt]) { diff = -10; }
+            else { diff = 0; }
+            present_position[cnt]+=diff;
+            dxl_wb.goalPosition(dxl_id[cnt], (int32_t)present_position[cnt]);
+        }
     }
+    Serial.println("END");
 
     while(1){
         while(Serial.available()) {
             buffer[bufferIndex]  = Serial.read();
             bufferIndex++;
         }
-        char num = buffer[0];
+        char num = buffer[0];;
         int degree = atoi(&buffer[2]);
         switch (num) {
         case 0:
@@ -104,27 +120,42 @@ void setup() {
         case '1':
             Serial.println(num);
             Serial.println(degree);
-            
-            for(int e = 2250;e<=degree;e+= 1) {
+            if(present_position[0] < degree) { diff=1;}
+            else { diff = -1; }
+            for(e = present_position[0]; degree != e; e+= diff) {
                 dxl_wb.goalPosition(dxl_id[0], (int32_t)e);
-                delay(1);
+                present_position[0] = e;
             }
-            //dxl_wb.goalPosition(dxl_id[0], (int32_t)degree);
             break;
         case '2':
             Serial.println(num);
             Serial.println(degree);
-            dxl_wb.goalPosition(dxl_id[1], (int32_t)degree);
+            if(present_position[1] < degree) { diff=1;}
+            else { diff = -1; }
+            for(e = present_position[1]; degree != e; e+= diff) {
+                dxl_wb.goalPosition(dxl_id[1], (int32_t)e);
+                present_position[1] = e;
+            }
             break;
         case '3':
             Serial.println(num);
             Serial.println(degree);
-            dxl_wb.goalPosition(dxl_id[2], (int32_t)degree);
+            if(present_position[2] < degree) { diff=1;}
+            else { diff = -1; }
+            for(e = present_position[2]; degree != e; e+= diff) {
+                dxl_wb.goalPosition(dxl_id[2], (int32_t)e);
+                present_position[2] = e;
+            }
             break;
         case '4':
             Serial.println(num);
             Serial.println(degree);
-            dxl_wb.goalPosition(dxl_id[3], (int32_t)degree);
+            if(present_position[3] < degree) { diff=1;}
+            else { diff = -1; }
+            for(e = present_position[3]; degree != e; e+= diff) {
+                dxl_wb.goalPosition(dxl_id[3], (int32_t)e);
+                present_position[3] = e;
+            }
             break;
         case '5':
             Serial.println(num);
@@ -133,9 +164,56 @@ void setup() {
             break;
         case 'i':
             Serial.println("Init State");
-            for (int cnt=SCNT; cnt < NCNT; cnt++) {
-                dxl_wb.goalPosition(dxl_id[cnt], init_position[cnt]);
+            loop_num = getMax(present_position, init_position, NCNT);
+            Serial.println(loop_num);
+            for (e = 0; e<loop_num; e+=10) {
+                for (int cnt=SCNT; cnt < NCNT; cnt++) {
+                    if(present_position[cnt] < init_position[cnt]) { diff=10;}
+                    else if(present_position[cnt] > init_position[cnt]) { diff = -10; }
+                    else { diff = 0; }
+                    present_position[cnt]+=diff;
+                    dxl_wb.goalPosition(dxl_id[cnt], (int32_t)present_position[cnt]);
+                }
             }
+            Serial.println("END");
+            break;
+        case 'd':
+            Serial.println("Grab State");
+            loop_num = getMax(present_position, grab_position, NCNT);
+            dxl_wb.goalPosition(dxl_id[4], (int32_t)grab_position[4]);
+            Serial.println(loop_num);
+            for (e = 0; e<loop_num; e+=10) {
+                for (int cnt=SCNT; cnt < NCNT-1; cnt++) {
+                    if(present_position[cnt] < grab_position[cnt]) { diff=10;}
+                    else if(present_position[cnt] > grab_position[cnt]) { diff = -10; }
+                    else { diff = 0; }
+                    present_position[cnt]+=diff;
+                    dxl_wb.goalPosition(dxl_id[cnt], (int32_t)present_position[cnt]);
+                }
+            }
+            delay(2000);
+            dxl_wb.goalPosition(dxl_id[4], (int32_t)1400);
+            present_position[4] = 1400;
+            Serial.println("END");
+
+            
+            if(present_position[3] < 2500) { diff=1;}
+            else { diff = -1; }
+            for(e = present_position[3]; 2500 != e; e+= diff) {
+                dxl_wb.goalPosition(dxl_id[3], (int32_t)e);
+                present_position[3] = e;
+            }
+            delay(100);
+
+            if(present_position[0] < 2700) { diff=1;}
+            else { diff = -1; }
+            for(e = present_position[0]; 2700 != e; e+= diff) {
+                dxl_wb.goalPosition(dxl_id[0], (int32_t)e);
+                present_position[0] = e;
+            }
+            delay(1000);
+            dxl_wb.goalPosition(dxl_id[4], (int32_t)1900);
+            Serial.println("END");
             break;
         case 'g':
             for (int cnt=SCNT; cnt < NCNT; cnt++) {
@@ -161,6 +239,16 @@ void setup() {
         bufferIndex = 0;
         delay(1000);
     }
+}
+
+int getMax(int32_t* arr1, int32_t* arr2, int arr_len) {
+    int max = 0;
+    int buf = 0;
+    for (int i=0; i<arr_len; i++) {
+        buf = abs(arr1[i]-arr2[i]);
+        if(max < buf) { max =buf; }
+    }
+    return max;
 }
 
 void loop() {
